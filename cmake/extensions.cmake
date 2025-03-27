b54a192324d48267ab796f6b04f2628846ecfb1b
Marcel Graber <marcel@clever.design>
2025-03-27 12:26:36 +0100

    fix: remove unused global interface buidl

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -1,27 +1,3 @@
-# 1.2 tiny_library_*
-#
-# Tiny libraries use CMake's library concept and a set of assumptions about how
-# zephyr code is organized to cut down on boilerplate code.
-#
-# A Tiny library can be constructed by the function tiny_library or
-# tiny_library_named. The constructors create a CMake library with a name
-# accessible through the variable TINY_CURRENT_LIBRARY.
-#
-# The variable TINY_CURRENT_LIBRARY should seldom be needed since the zephyr
-# libraries have methods that modify the libraries. These methods have the
-# signature: tiny_library_<target-function>
-#
-# The methods are wrappers around the CMake target_* functions. See
-# https://cmake.org/cmake/help/latest/manual/cmake-commands.7.html for
-# documentation on the underlying target_* functions.
-#
-# The methods modify the CMake target_* API to reduce boilerplate; PRIVATE is
-# assumed The target is assumed to be TINY_CURRENT_LIBRARY
-#
-# When a flag that is given through the tiny_* API conflicts with the
-# tiny_library_* API then precedence will be given to the tiny_library_* API. In
-# other words, local configuration overrides global configuration.
-
 # Constructor with an explicitly given name.
 macro(tiny_library_named name)
   # This is a macro because we need add_library() to be executed within the
@@ -30,8 +6,6 @@ macro(tiny_library_named name)
   add_library(${name} STATIC "")
 
   tiny_append_cmake_library(${name})
-
-  target_link_libraries(${name} PUBLIC tiny_interface)
 endmacro()
 
 #
@@ -45,6 +19,10 @@ endfunction()
 
 function(tiny_library_include_directories)
   target_include_directories(${TINY_CURRENT_LIBRARY} PRIVATE ${ARGN})
+endfunction()
+
+function(tiny_library_include_directories_public)
+  target_include_directories(${TINY_CURRENT_LIBRARY} PUBLIC ${ARGN})
 endfunction()
 
 function(tiny_library_link_libraries item)
@@ -71,46 +49,13 @@ function(tiny_append_cmake_library library)
   set_property(GLOBAL APPEND PROPERTY TINY_LIBS ${library})
 endfunction()
 
-# https://cmake.org/cmake/help/latest/command/target_link_libraries.html
-function(tiny_link_libraries)
-  if(ARGV0 STREQUAL "PROPERTY")
-    set(property $<TARGET_PROPERTY:linker,${ARGV1}>)
-    set(property_defined $<BOOL:${property}>)
-    if(ARGC GREATER 3)
-      message(FATAL_ERROR "tiny_link_options(PROPERTY <prop> [<val>]) "
-                          "called with too many arguments.")
-    elseif(ARGC EQUAL 3)
-      target_link_libraries(
-        tiny_interface INTERFACE $<${property_defined}:${property}${ARGV2}>)
-    else()
-      target_link_libraries(tiny_interface INTERFACE ${property})
-    endif()
-  else()
-    target_link_libraries(tiny_interface INTERFACE ${ARGV})
-  endif()
-endfunction()
-
-function(tiny_sources)
-  foreach(arg ${ARGV})
-    if(IS_DIRECTORY ${arg})
-      message(FATAL_ERROR "tiny_sources() was called on a directory")
-    endif()
-    target_sources(tiny PRIVATE ${arg})
-  endforeach()
-endfunction()
-
-function(tiny_sources_ifdef feature_toggle)
+function(tiny_library_sources_ifdef feature_toggle)
   if(${${feature_toggle}})
-    tiny_sources(${ARGN})
+    tiny_library_sources(${ARGN})
   endif()
-endfunction()
-
-# https://cmake.org/cmake/help/latest/command/target_include_directories.html
-function(tiny_include_directories)
-  target_include_directories(tiny_interface INTERFACE ${ARGV})
 endfunction()
 
 # https://cmake.org/cmake/help/latest/command/target_compile_definitions.html
-function(tiny_compile_definitions)
-  target_compile_definitions(tiny_interface INTERFACE ${ARGV})
+function(tiny_library_compile_definitions)
+  target_compile_definitions(${TINY_CURRENT_LIBRARY} INTERFACE ${ARGV})
 endfunction()
